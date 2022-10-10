90db55ada6f2f90c0d881075a614e1a04032f3e1
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-10-10 10:28:01 +0300

    Create snake of 3 square segments

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 
 
+for i in range(3):
+    snake_segment = Turtle("square")
+    snake_segment.color("white")
+    snake_segment.goto(-20 * i, 0)
+
 
 
 screen.exitonclick()
