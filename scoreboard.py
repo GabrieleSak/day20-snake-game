d7c8de8251b10217b605fc92aea183db381a80dc
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-10-10 20:32:16 +0300

    Add Scoreboard class and score calculation

---
+++ scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier New", 12, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.hideturtle()
+        self.penup()
+        self.color("white")
+        self.goto(0, 260)
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
